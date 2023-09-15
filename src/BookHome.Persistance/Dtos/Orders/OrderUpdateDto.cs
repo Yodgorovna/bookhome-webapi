150f6dfa543e09cc407a8c708b0be47de7933c43
Yodgorovna <lobargoziyeva9@gmail.com>
2023-09-15 10:33:36 +0500

    Dtos is complete

---
+++ src/BookHome.Persistance/Dtos/Orders/OrderUpdateDto.cs
@@ -0,0 +1,26 @@
+﻿namespace BookHome.Persistance.Dtos.Orders;
+
+public class OrderUpdateDto
+{
+    public long UserId { get; set; }
+
+    public string Status { get; set; } = string.Empty;
+
+    public double BookPrice { get; set; }
+
+    public double DeliveryPrice { get; set; }
+
+    public double ResultPrice { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public string PaymentType { get; set; } = string.Empty;
+
+    public bool IsPaid { get; set; }
+
+    public bool IsContracted { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+}
---
+++ src/BookHome.Persistance/Dtos/Users/UserCreateDto.cs
@@ -0,0 +1,34 @@
+﻿using BookHome.Domain.Enums;
+
+namespace BookHome.Persistance.Dtos.Users;
+
+public class UserCreateDto
+{
+    public string FirstName { get; set; } = string.Empty;
+
+    public string LastName { get; set; } = string.Empty;
+
+    public string PhoneNumber { get; set; } = string.Empty;
+
+    public string PassportSeriaNumber { get; set; } = string.Empty;
+
+    public bool IsMale { get; set; }
+
+    public DateTime BirthDate { get; set; }
+
+    public string Country { get; set; } = string.Empty;
+
+    public string Region { get; set; } = string.Empty;
+
+    public string District { get; set; } = string.Empty;
+
+    public string Address { get; set; } = string.Empty;
+
+    public string Password { get; set; } = string.Empty;
+
+    public string ImagePath { get; set; } = string.Empty;
+
+    public DateTime LastActivity { get; set; }
+
+    public IdentityRole IdentityRole { get; set; }
+}
