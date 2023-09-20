8cf4ff8df4bdfa950b45a853e1f0a8de9ace9661
Yodgorovna <lobargoziyeva9@gmail.com>
2023-09-20 11:39:22 +0500

    .

--- src/BookHome.Persistance/Validators/Dtos/Books/BookImageUpdateValidator.cs
+++ src/BookHome.Persistance/Validators/Dtos/Books/BookImageUpdateValidator.cs
@@ -11,11 +11,11 @@ namespace BookHome.Persistance.Validators.Dtos.Books
             int maxImageSizeMB = 3;
             RuleFor(dto => dto.ImagePath).NotEmpty().NotNull().WithMessage("Image field is required");
             RuleFor(dto => dto.ImagePath.Length).LessThan(maxImageSizeMB * 1024 * 1024 + 1).WithMessage($"Image size must be less than {maxImageSizeMB} MB");
-            RuleFor(dto => dto.ImagePath).Must(predicate =>
-            {
-                FileInfo fileInfo = new FileInfo(predicate);
-                return MediaHelper.GetImageExtensions().Contains(fileInfo.Extension);
-            }).WithMessage("This file type is not image file");
+            //RuleFor(dto => dto.ImagePath).Must(predicate =>
+            //{
+            //    FileInfo fileInfo = new FileInfo(predicate);
+            //    return MediaHelper.GetImageExtensions().Contains(fileInfo.Extension);
+            //}).WithMessage("This file type is not image file");
         }
     }
 }
